da1610df8ba0775851e7381f491d7a4484844464
Hhhhhhhao <haoc3@andrew.cmu.edu>
2022-10-05 19:45:25 -0400

    [Feature] Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='semilearn',
-    version='0.2.0',
+    version='0.2.1',
     description='Unfied Semi-Supervised Learning Benchmark',
     long_description=long_description,
     long_description_content_type='text/markdown',
