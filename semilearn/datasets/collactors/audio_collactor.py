baf8305adc280bc5e8955287d33dbc09aa904715
Hugo Schmutz <hugo.schmutz@eleves.enpc.fr>
2023-06-28 12:01:25 +0200

    fix bug in audo collactor

--- semilearn/datasets/collactors/audio_collactor.py
+++ semilearn/datasets/collactors/audio_collactor.py
@@ -70,7 +70,7 @@ class DataCollatorWithPadding:
             
         if 'labels' in batch:
             if len(s_features)==0: 
-                return {'idx_lb': batch['idx'].long(), 'x_lb': batch['input_values'], 'y_lb': batch['label'].long()}
+                return {'idx_lb': batch['idx'].long(), 'x_lb': batch['input_values'], 'y_lb': batch['labels'].long()}
             else:
                 s_batch = self.tokenizer(
                         s_features,
@@ -81,7 +81,7 @@ class DataCollatorWithPadding:
                         return_tensors=self.return_tensors,
                         truncation=True,
                     )
-                return {'idx_lb': batch['idx'].long(), 'x_lb': batch['input_values'], 'x_lb_s': s_batch['input_values'], 'y_lb': batch['label'].long()}
+                return {'idx_lb': batch['idx'].long(), 'x_lb': batch['input_values'], 'x_lb_s': s_batch['input_values'], 'y_lb': batch['labels'].long()}
         else:
             if len(s_features) > 0:
                 s_batch = self.tokenizer(
