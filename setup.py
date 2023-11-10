a8ec1d7b2c3bee96f490acc7745311ce6c6d1584
Hao Chen <haoc3@andrew.cmu.edu>
2023-11-10 15:50:05 -0500

    [Update] Release semilearn==0.3.2 for Pytorch Tutorial

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with pathlib.Path('requirements.txt').open() as requirements_txt:
 
 setup(
     name='semilearn',
-    version='0.3.1',
+    version='0.3.2',
     description='Unfied Semi-Supervised Learning Benchmark',
     long_description=long_description,
     long_description_content_type='text/markdown',
