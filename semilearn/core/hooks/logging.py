b5a61a3ba3198064ee8c2b45648235dc854aeb37
harit7 <harit.vishwakarma@gmail.com>
2024-04-09 17:47:19 -0500

    integrating

--- semilearn/core/hooks/logging.py
+++ semilearn/core/hooks/logging.py
@@ -26,6 +26,7 @@ class LoggingHook(Hook):
             
             if not algorithm.tb_log is None:
                 algorithm.tb_log.update(algorithm.log_dict, algorithm.it)
+
         
         elif self.every_n_iters(algorithm, algorithm.num_log_iter):
             if not algorithm.distributed or (algorithm.distributed and algorithm.rank % algorithm.ngpus_per_node == 0):
