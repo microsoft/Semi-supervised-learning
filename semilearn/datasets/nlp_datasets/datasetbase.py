794e6d1a346523c851fb1819fb0e4662e5945178
NamburiSrinath <namburisrinath@gmail.com>
2024-04-18 00:02:37 -0500

    x_ulb for nlp dataset WIP

--- semilearn/datasets/nlp_datasets/datasetbase.py
+++ semilearn/datasets/nlp_datasets/datasetbase.py
@@ -59,22 +59,33 @@ class BasicDataset(Dataset):
             target = target_ if not self.onehot else get_onehot(self.num_classes, target_)
 
         sen = self.data[idx]
+
+        # TODO: Add x_ulb and return it, throwing unknown error currently!
+        if self.transform is not None:
+            x_ulb = self.transform.ToTensor()(sen)
+
         # set augmented images
         if self.is_ulb == False:
-            return {'idx':idx, 'text':sen[0], 'label':target} 
+            # return {'idx':idx, 'text':sen[0],'label':target}
+            return {'idx':idx, 'text':sen[0],'label':target, 'x_ulb':x_ulb} 
         else:
             if self.alg == 'fullysupervised' or self.alg == 'supervised':
-                return {'idx':idx, 'text':sen[0], 'label':target} 
+                # return {'idx':idx, 'text':sen[0], 'label':target} 
+                return {'idx':idx, 'text':sen[0], 'label':target, 'x_ulb':x_ulb} 
             if self.alg == 'pseudolabel' or self.alg == 'vat':
-                return {'idx':idx, 'text':sen[0]} 
+                # return {'idx':idx, 'text':sen[0]}
+                return {'idx':idx, 'text':sen[0], 'x_ulb':x_ulb} 
             elif self.alg == 'pimodel' or self.alg == 'meanteacher' or self.alg == 'mixmatch':
-                return {'idx':idx, 'text':sen[0], 'text_s':sen[0]}
+                # return {'idx':idx, 'text':sen[0], 'text_s':sen[0]}
+                return {'idx':idx, 'text':sen[0], 'text_s':sen[0], 'x_ulb':x_ulb}
             elif self.alg == 'comatch' or self.alg == 'remixmatch':
                 indices = [1, 2]
                 np.random.shuffle(indices)
-                return {'idx':idx, 'text':sen[0], 'text_s':sen[indices[0]], 'text_s_':sen[indices[1]]}
+                # return {'idx':idx, 'text':sen[0], 'text_s':sen[indices[0]], 'text_s_':sen[indices[1]]}
+                return {'idx':idx, 'text':sen[0], 'text_s':sen[indices[0]], 'text_s_':sen[indices[1]], 'x_ulb':x_ulb}
             else:
-                return {'idx':idx, 'text':sen[0], 'text_s':sen[self.random_choose_sen()]}
+                # return {'idx':idx, 'text':sen[0], 'text_s':sen[self.random_choose_sen()]}
+                return {'idx':idx, 'text':sen[0], 'text_s':sen[self.random_choose_sen()], 'x_ulb':x_ulb}
                 
     def __len__(self):
         return len(self.data)
