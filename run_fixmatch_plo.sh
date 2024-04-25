e4d2d1a5259d6c34de2d6ddbec5bc9cc9f89954d
Reid Chen <reid@deepneural.network>
2024-04-24 22:05:01 -0500

    update

--- run_fixmatch_plo.sh
+++ run_fixmatch_plo.sh
@@ -1,1 +1,1 @@
-python train.py --c config/usb_cv/fixmatch/fixmatch_cifar100_200_0.yaml --use_post_hoc_calib True --n_cal 1000 --n_th 1000 --take_d_cal_th_from eval --loss_reweight False --aug_1 weak --aug_2 strong
+python train.py --c config/usb_cv/fixmatch/fixmatch_stl10_40_0.yaml --use_post_hoc_calib False --n_cal 1000 --n_th 1000 --take_d_cal_th_from eval --loss_reweight False --aug_1 weak --aug_2 strong
--- run_tensorboard.sh
+++ run_tensorboard.sh
@@ -1,2 +1,1 @@
-tensorboard --logdir=./saved_models --port 4443
-
+tensorboard --logdir=./saved_models --port 4443 --bind_all
