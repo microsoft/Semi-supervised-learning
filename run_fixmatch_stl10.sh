4e4ae3b816efa9cbc9a99cfbe5afca48684392bd
Reid Chen <reid@deepneural.network>
2024-05-08 02:46:11 -0500

    stl 5000

--- run_fixmatch_stl10.sh
+++ run_fixmatch_stl10.sh
@@ -1,4 +1,4 @@
 # vanilla
-python train.py --c config/classic_cv/fixmatch/fixmatch_stl10_10000_0.yaml --use_post_hoc_calib False --n_cal 2400 --n_th 2400 --take_d_cal_th_from eval --loss_reweight False --aug_1 weak --aug_2 strong --gpu 0 --accumulate_pseudo_labels True
+python train.py --c config/classic_cv/fixmatch/fixmatch_stl10_5000_0.yaml --use_post_hoc_calib False --n_cal 2400 --n_th 2400 --take_d_cal_th_from eval --loss_reweight False --aug_1 weak --aug_2 strong --gpu 0 --accumulate_pseudo_labels True
 # with PLO
-python train.py --c config/classic_cv/fixmatch/fixmatch_stl10_10000_0.yaml --use_post_hoc_calib True --n_cal 2400 --n_th 2400 --take_d_cal_th_from eval --loss_reweight False --aug_1 weak --aug_2 strong --gpu 0 --accumulate_pseudo_labels True
+python train.py --c config/classic_cv/fixmatch/fixmatch_stl10_5000_0.yaml --use_post_hoc_calib True --n_cal 2400 --n_th 2400 --take_d_cal_th_from eval --loss_reweight False --aug_1 weak --aug_2 strong --gpu 0 --accumulate_pseudo_labels True
